fbc58f95d452d3337f56851d4588c627a0314973
sorrycc <572374929@qq.com>
2021-10-15 16:21:38 +0800

    自定义hook useDebounce去抖

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,8 +2,8 @@ import React from "react"
 import { SearchPanel } from "./search-panel"
 import { List } from './list'
 import { useEffect, useState } from 'react'
-import * as qs from 'qs'
-import { cleanObject } from '../../utils'
+import * as qs from 'qs'//不用再写name=tom&sex=男。。。
+import { cleanObject, useDebounce, useMount } from '../../utils'
 
 const apiUrl = process.env.REACT_APP_API_URL
 
@@ -15,22 +15,23 @@ export const ProjectListScreen = () => {
     personId: ''
   })
   const [list, setList] = useState([])
+  const debouncedParam = useDebounce(param, 2000)
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
       if (response.ok) {//请求成功
         setList(await response.json())
       }
     })
-  }, [param])
+  }, [debouncedParam])
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if (response.ok) {//请求成功
         setUsers(await response.json())
       }
     })
-  })
+  }, [])
 
   return <div>
     <SearchPanel param={param} setParam={setParam} users={users} />
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 export const isFalsy = (value) => value === 0 ? true : !!value
 
 export const cleanObject = (object) => {
@@ -9,4 +10,24 @@ export const cleanObject = (object) => {
     }
   })
   return result
+}
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback()
+  }, [])
+}
+
+export const useDebounce = (value, delay) => {//去抖 
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    //value变化后设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay)
+    //useEffect的匿名函数中return匿名函数的相当于componentDidunMount，进行卸载任务
+    //每次在上一个useEffect处理完以后在运行
+    return () => clearTimeout(timeout)
+  }, [value, delay])
+
+  return debouncedValue
 }
